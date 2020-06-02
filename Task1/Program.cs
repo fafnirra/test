501b531a1224176a5371f9fc7ae3459fb71066c3
Anastasya Lukovskaya <fafnirra@gmail.com>
2020-06-02 10:41:05 +0300

    Test 1 made

---
+++ Task1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        public static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+
+        static void Main(string[] args)
+        {
+            int akl = 5;
+            int gb = 4;
+            Console.WriteLine(Sum(akl, gb));
+        }
+    }
+}
